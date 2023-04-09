d68843bd9f0c0a2090e2adfc530ef54c3a8bd0f4
3V1LXD <50510176+3V1LXD@users.noreply.github.com>
2023-04-09 13:29:38 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,110 @@
+<p align="center">
+  <img src="src/assets/ROT.png" alt="ROT Logo" />
+</p>
+
+# ROT - Game Input Emulation Script
+
+ROT is a Python script that demonstrates how to use the vgamepad library to emulate an Xbox 360 game controller for educational purposes. It reads configurations for key bindings and rotation sequences from separate files, allowing for customization and exploration of game mechanics. This project is intended to help users learn programming concepts, input emulation, and understand in-game mechanics.
+
+## Disclaimer
+
+This script is for educational purposes only and is not intended for use in violation of any terms of service or end-user license agreement (EULA) associated with any specific game. The author does not encourage or condone the use of this script to cheat, exploit, or gain an unfair advantage in any game. Please use this script responsibly and in compliance with the rules and policies of the game you are playing.
+
+## Educational Value
+
+The primary goal of this project is to provide a learning resource for users interested in understanding:
+
+1. How to interact with the vgamepad library to create virtual gamepad inputs.
+2. The use of the configparser library to read configuration files for key bindings and rotation sequences.
+3. The implementation of multi-threading to run separate processes concurrently.
+4. The use of the keyboard library to create hotkeys for script control.
+
+Please utilize this script in a manner that promotes learning and understanding of the underlying concepts without violating any terms of service, EULA, or other legal and ethical guidelines.
+
+## Usage
+
+Before using this script, please ensure you have the vgamepad library installed, a 'config.ini' file set up with key bindings, and a separate folder called 'rotations' containing rotation configuration files for different game classes or builds.
+
+When running the script, it will prompt you to press a specific hotkey combination to toggle the combat rotation on and off. Note that this functionality is provided for educational purposes and should not be used to gain an unfair advantage or violate the terms of service of any game.
+
+## Contributing
+
+Contributions to this project should focus on improving its educational value or expanding its capabilities for learning and experimentation, without promoting or enabling cheating or exploitation in any game.
+
+## Installation and Setup
+
+Follow these steps to install the required libraries and set up the necessary tools to use this script.
+
+### Prerequisites
+
+1. Install Python (3.6 or higher) from [python.org](https://www.python.org/downloads/).
+
+2. Install the following Python libraries using pip:
+
+`pip install vgamepad keyboard configparser`
+or
+`pip install -r requirements.txt`
+
+### Setting Up Your Game
+
+For games such as World of Warcraft, you will need to install an addon that allows you to use a physical game controller to control character movement and abilities. This script will emulate a second virtual Xbox 360 controller, allowing you to use the physical controller for movement and the virtual controller for abilities.
+
+Download and install the ConsolePort addon for World of Warcraft from [CurseForge](https://www.curseforge.com/wow/addons/console-port) or your preferred addon manager.
+
+This application is not specifically designed for any game but can be used with any game that supports the use of a physical game controller. If you are using a game other than World of Warcraft, you may need to install an additional addon or other software to enable controller support. World of Warcraft is just one example of a game that the script can be used with.
+
+### Setting Up Controllers
+
+To use your own controller for character movement while also using this script, follow these steps:
+
+1. Ensure your physical game controller (e.g., Xbox, PlayStation, or another compatible controller) is connected to your computer before running the script.
+
+2. Start your game with any addons needed to enable controller support.
+
+3. After connecting your physical controller and enabling the addon, run the script. The script will emulate a second virtual Xbox 360 controller, allowing the script to control character abilities.
+
+By connecting your physical controller before running the script, you ensure that World of Warcraft detects it as the first controller, allowing you to retain full control over character movement. Otherwise, controller movement will be disabled while the script is running and an MNK must be used to move the character.
+
+## Usage
+
+1. Ensure you have completed the installation process and properly set up the controller as described in the [Installation and Setup](#installation-and-setup) section.
+
+2. Create a folder called `bindings` in the script's directory and add game configuration files for different games and their key bindings. An example can be found in the repository.
+
+3. Create a folder called `rotations` in the script's directory and add rotation configuration files for different game classes or builds. An example can be found in the repository.
+
+4. Run the script using the following command:
+
+`python ROT.py`
+
+5. The script will prompt you to press a specific hotkey combination (e.g., 'DEL') to toggle the combat rotation on and off. Press the hotkey to start or stop the script's ability control. I chose 'DEL' because it is easy for me. You can choose any key you want by changing the value of the `TOGGLE_ROTATION` constant in the script's application variables.
+
+6. To exit the script, press the specified hotkey combination for exit (e.g., 'Shift+X'). You can change this by changing the value of the `EXIT_PROGRAM` constant in the script.
+
+Please note that the usage of this script for automating gameplay, especially in a manner that violates a game's terms of service or EULA, is not recommended. Use this script responsibly, and focus on the educational value and understanding of the underlying concepts.
+
+## Support and Community
+
+If you need help or have questions about this application, the best way to get support is by joining the Discord.
+
+To join the Discord, click on this invite link: [Discord](https://discord.com/invite/aP9CjWE)
+
+## Support and Donations
+
+If you enjoy using this project and find it helpful, please consider supporting its development. Your support helps to ensure the project's continued development, bug fixes, and improvements.
+
+### Other Ways to Support
+
+- Share this project.
+- Report any issues you encounter or suggest new features and improvements by creating a new issue on the GitHub repository.
+- Contribute to the project by submitting pull requests with bug fixes, new features, or improvements to the code or documentation.
+- Star the repository on GitHub to show your appreciation for the project.
+- Show your support on social media. [Linktree](https://linktr.ee/3v1lxd)
+
+### Donations
+
+If you would like to make a financial contribution to support development, you can donate using the following method:
+
+- [PayPal](https://paypal.me/ScottDIT)
+
+Your donation, no matter the size, is greatly appreciated and will help to support future development and maintenance. Thank you for your generosity!
---
+++ src/ROT.py
@@ -0,0 +1,263 @@
+import vgamepad as vg
+import configparser
+import threading
+import keyboard
+import time
+
+#############################################
+# Application Variables
+
+# Game bindings to load, change and reload app to switch bindings
+BINDINGS_FILE = 'bindings/WOW.ini'
+# Rotation file to load, change and reload app to switch rotations
+ROTATION_FILE = 'rotations/WOW_warrior_arms.ini'
+# Delay between each ability in the rotation
+ABILITY_DELAY = 0.1
+# Delay between each rotation loop
+KEEP_RUNNING_DELAY = 1
+# Delay between each run loop to detect hotkeys and reduce CPU usage.
+RUN_LOOP_DELAY = 1
+# Delay to wait for button presses to be registered
+BUTTON_DELAY = 0.1
+
+# Hotkeys
+# Refer to the keyboard docs for information on hotkeys and their syntax.
+# https://github.com/boppreh/keyboard#keyboard.add_hotkey
+# Toggle the rotation
+TOGGLE_ROTATION = 'DEL'
+# Exit the application
+EXIT_PROGRAM = 'shift+x'
+
+#############################################
+# Gamepad Controls
+
+gamepad = vg.VX360Gamepad()
+
+# Gamepad button constants
+# Constants for each button on the gamepad, used for easier identification and use in the code.
+DPAD_UP = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_UP
+DPAD_DOWN = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_DOWN
+DPAD_LEFT = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_LEFT
+DPAD_RIGHT = vg.XUSB_BUTTON.XUSB_GAMEPAD_DPAD_RIGHT
+START = vg.XUSB_BUTTON.XUSB_GAMEPAD_START
+BACK = vg.XUSB_BUTTON.XUSB_GAMEPAD_BACK
+LEFT_THUMB = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_THUMB
+RIGHT_THUMB = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_THUMB
+LEFT_SHOULDER = vg.XUSB_BUTTON.XUSB_GAMEPAD_LEFT_SHOULDER
+RIGHT_SHOULDER = vg.XUSB_BUTTON.XUSB_GAMEPAD_RIGHT_SHOULDER
+GUIDE = vg.XUSB_BUTTON.XUSB_GAMEPAD_GUIDE
+A = vg.XUSB_BUTTON.XUSB_GAMEPAD_A
+B = vg.XUSB_BUTTON.XUSB_GAMEPAD_B
+X = vg.XUSB_BUTTON.XUSB_GAMEPAD_X
+Y = vg.XUSB_BUTTON.XUSB_GAMEPAD_Y
+
+# Dictionary mapping button names to their corresponding constants
+gpad = {
+    'DPAD_UP': DPAD_UP,
+    'DPAD_DOWN': DPAD_DOWN,
+    'DPAD_LEFT': DPAD_LEFT,
+    'DPAD_RIGHT': DPAD_RIGHT,
+    'START': START,
+    'BACK': BACK,
+    'LEFT_THUMB': LEFT_THUMB,
+    'RIGHT_THUMB': RIGHT_THUMB,
+    'LEFT_SHOULDER': LEFT_SHOULDER,
+    'RIGHT_SHOULDER': RIGHT_SHOULDER,
+    'GUIDE': GUIDE,
+    'A': A,
+    'B': B,
+    'X': X,
+    'Y': Y
+}
+
+
+def left_trigger():
+    """
+    Fully press the left trigger on the gamepad.
+    """
+    gamepad.left_trigger(value=255)
+
+
+def lt_release():
+    """
+    Release the left trigger on the gamepad.
+    """
+    gamepad.left_trigger(value=0)
+
+
+def right_trigger():
+    """
+    Fully press the right trigger on the gamepad.
+    """
+    gamepad.right_trigger(value=255)
+
+
+def rt_release():
+    """
+    Release the right trigger on the gamepad.
+    """
+    gamepad.right_trigger(value=0)
+
+
+def lr_trigger():
+    """
+    Fully press both triggers on the gamepad.
+    """
+    gamepad.left_trigger(value=255)
+    gamepad.right_trigger(value=255)
+
+
+def lrt_release():
+    """
+    Release both triggers on the gamepad.
+    """
+    gamepad.left_trigger(value=0)
+    gamepad.right_trigger(value=0)
+
+
+def press(button):
+    """
+    Press a button on the gamepad.
+    :param button: The button to press.
+    """
+    gamepad.press_button(button=button)
+
+
+def update():
+    """
+    Update the gamepad state.
+    """
+    gamepad.update()
+
+
+def reset():
+    """
+    Reset the gamepad state by releasing all buttons and triggers.
+    """
+    gamepad.reset()
+#############################################
+
+
+# Get bindings from bindings file
+config = configparser.ConfigParser()
+config.read(BINDINGS_FILE)
+
+buttons = {}
+for button, ability in config['bindings'].items():
+    # check if ability is bound to a button
+    if ability != 'None':
+        buttons[ability] = button.upper()
+
+# Flags to control the while loop and background thread
+keep_running = False
+thread_running = True
+
+
+def run_loop():
+    """
+    Function that runs the main loop for executing the rotation.
+    The loop runs indefinitely as long as thread_running is True, but the rotation execution is controlled by the keep_running flag.
+    """
+    global keep_running, thread_running
+    while thread_running:
+        if keep_running:
+            print("Running rotation...")
+            # load new config file from rotations folder
+            config.read(ROTATION_FILE)
+            rotation = config['ROT']['Rotation'].split('\n')
+            # spam rotation
+            for ability in rotation:
+                if keep_running:
+                    if ', ' in ability:
+                        ability_delay = int(ability.split(', ')[1]) / 1000
+                        ability = ability.split(', ')[0]
+                    else:
+                        ability_delay = ABILITY_DELAY
+
+                    for bound_ability, button in buttons.items():
+                        if bound_ability in ability:
+                            # check if button includes a trigger pull
+                            if 'LT_' in button:
+                                left_trigger()
+                            elif 'RT_' in button:
+                                right_trigger()
+                            elif 'LTRT_' in button:
+                                lr_trigger()
+                            else:
+                                lrt_release()
+
+                            # wait for trigger to be pulled or released
+                            # vgamepad doesn't seem to support multiple presses/releases in a single update
+                            # so we need to wait for the trigger before pressing another button
+                            update()
+                            time.sleep(BUTTON_DELAY)
+
+                            if button != 'RIGHT_SHOULDER' and button != 'LEFT_SHOULDER' and button != 'RIGHT_THUMB' and button != 'LEFT_THUMB' and button != 'START' and button != 'BACK' and button != 'GUIDE' and button != 'DPAD_UP' and button != 'DPAD_DOWN' and button != 'DPAD_LEFT' and button != 'DPAD_RIGHT':
+                                if '_' in button:
+                                    button = '_'.join(button.split('_')[1:])
+
+                            press(gpad[button])
+                            print('Activating ' + bound_ability +
+                                  ' with ' + button)
+
+                            # wait for ability delay
+                            update()
+                            time.sleep(ability_delay)
+
+                            # wait for button to be released
+                            reset()
+                            update()
+                            time.sleep(BUTTON_DELAY)
+
+            # Adjust this delay as needed to reduce CPU usage when the loop is running
+            time.sleep(KEEP_RUNNING_DELAY)
+        else:
+            # Adjust this delay as needed to reduce CPU usage when the loop is not running
+            time.sleep(RUN_LOOP_DELAY)
+
+
+def toggle_loop():
+    """
+    Function that toggles the main loop's keep_running flag when the hotkey is pressed.
+    """
+    global keep_running
+    keep_running = not keep_running
+    print("Starting rotation" if keep_running else "Stopping rotation")
+    if not keep_running:
+        # reset controller on stop
+        lrt_release()
+        update()
+        reset()
+        update()
+
+
+def exit_program():
+    """
+    Function to stop the background thread and exit the script.
+    """
+    global thread_running
+    thread_running = False
+    print("Exiting...")
+
+
+def main():
+    """
+    Main function that initializes and starts the loop_thread and sets up the hotkeys for toggling the loop and exiting the program.
+    """
+    loop_thread = threading.Thread(target=run_loop)
+    loop_thread.start()
+
+    # Set up the hotkey to toggle the loop (for example, 'DEL')
+    keyboard.add_hotkey(TOGGLE_ROTATION, toggle_loop)
+    # Set up the hotkey to exit the program (for example, 'shift+x')
+    keyboard.add_hotkey(EXIT_PROGRAM, exit_program)
+
+    print(
+        f"Press '{TOGGLE_ROTATION}' to toggle the rotation. Press '{EXIT_PROGRAM}' to exit.")
+
+    # Keep the main thread running so the hotkeys can be detected
+    loop_thread.join()
+
+
+if __name__ == "__main__":
+    main()
